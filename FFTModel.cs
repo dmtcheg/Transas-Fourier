17bda7b3fc4b33c131174edbc3cde095a8bdfb29
Dmitry Chegodaev <cheg15032001@gmail.com>
2021-10-17 17:57:13 +0300

    fix charts

--- FFTModel.cs
+++ FFTModel.cs
@@ -13,37 +13,62 @@ namespace FourierTransas
 {
     public class FFTModel
     {
-        public PlotModel Plot { get; private set; }
+        public PlotModel Plot1 { get; private set; }
+        public PlotModel Plot2 { get; private set; }
+        public PlotModel Plot3 { get; private set; }
         public FFTModel()
         {
-            Plot = new PlotModel { Title = "sample" };
-            
-            var len = 2048; // freq
+            Plot1 = new PlotModel { Title = "chart 1" };
+            Plot2 = new PlotModel { Title = "chart 2" };
+            Plot3 = new PlotModel { Title = "chart 3" };
+
+            var len = 200000; // freq
             var sampleRate = 2 * len;
             var h1 = Generate.Sinusoidal(len, sampleRate, 60, 10);
             var h2 = Generate.Sinusoidal(len, sampleRate, 120, 20);
 
-            var samples = new Complex[len];
+            var h3 = Generate.Sinusoidal(len, sampleRate, 600, 64);
+            var h4 = Generate.Sinusoidal(len, sampleRate, 1200, 32);
+
+            var h5 = Generate.Sinusoidal(len, sampleRate, 3000, 48);
+            var h6 = Generate.Sinusoidal(len, sampleRate, 6000, 72);
+
+            var samples = new Complex[3][];
+            for (int i = 0; i < 3; i++)
+            {
+                samples[i] = new Complex[len];
+            }
             for (int i = 0; i < len; i++)
-                samples[i] = new Complex(h1[i] + h2[i], 0);
+            {
+                samples[0][i] = new Complex(h1[i] + h2[i], 0);
+                samples[1][i] = new Complex(h3[i] + h4[i], 0);
+                samples[2][i] = new Complex(h5[i] + h6[i], 0);
+            }
 
             // time -> freq
-            Fourier.Forward(samples, FourierOptions.NoScaling);
-
-            var magnitudes = new LineSeries();
-            var phases = new LineSeries();
-            for (int i = 0; i < samples.Length/2; i++)
+            for (int i = 0; i < 3; i++)
             {
-                // амплитуда
-                var magnitude = Math.Sqrt(Math.Pow(samples[i].Real, 2) + Math.Pow(samples[i].Imaginary, 2))*2/len;
-                // фаза
-                var phase = Math.Atan2(samples[i].Imaginary, samples[i].Real);
-
-                magnitudes.Points.Add(new DataPoint(2*i, magnitude));
-                phases.Points.Add(new DataPoint(2 * i, phase));
+                Fourier.Forward(samples[i], FourierOptions.NoScaling);
             }
-            Plot.Series.Add(magnitudes);
-            Plot.Series.Add(phases);
+
+            var magnitudes = new LineSeries[3];
+            for (int i = 0; i < 3; i++)
+            {
+                magnitudes[i] = new LineSeries();
+            }
+
+            for (int i = 0; i < len / 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    var magnitude = Math.Sqrt(Math.Pow(samples[j][i].Real, 2) + Math.Pow(samples[j][i].Imaginary, 2)) * 2 / len;
+                    magnitudes[j].Points.Add(new DataPoint(2 * i, magnitude));
+                }
+                //var phase = Math.Atan2(samples[i].Imaginary, samples[i].Real
+            }
+            Plot1.Series.Add(magnitudes[0]);
+            Plot2.Series.Add(magnitudes[1]);
+            Plot3.Series.Add(magnitudes[2]);
         }
     }
 }
