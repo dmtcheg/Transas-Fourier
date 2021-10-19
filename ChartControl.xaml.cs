2f9aeac00c52859079279f227749d948d1a3c122
Dmitry Chegodaev <cheg15032001@gmail.com>
2021-10-19 23:21:06 +0300

    emulate freeze

--- ChartControl.xaml.cs
+++ ChartControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using OxyPlot.Series;
+﻿using OxyPlot;
+using OxyPlot.Series;
+using System.Collections.Generic;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,14 +26,24 @@ namespace FourierTransas
             update = !update;
             var points = (Chart0.Model.Series[0] as LineSeries).Points;
             var len = points.Count;
-            while (update)
+            //var timer = new Timer(UpdateSeries, new object[] { Chart0, i++ }, 0, 1000);
+            for (int i = 0; i <len && update; i++)
             {
-                for (int i = 0; i < len; i++)
-                {
-                    points[i] = new OxyPlot.DataPoint(i, points[i].Y + 1);
-                    Chart0.InvalidatePlot();
-                }
+                points[i] = new DataPoint(i, points[i].Y + 1);
+                Chart0.Model.InvalidatePlot(true);
+                Thread.Sleep(100);
             }
+            //timer.Dispose();
+        }
+
+        private void UpdateSeries(object obj)
+        {
+            var info = obj as object[];
+            var view = info[0] as OxyPlot.Wpf.PlotView;
+            int i = (int)info[1];
+            var points = (view.Model.Series[0] as LineSeries).Points;
+            points[i] = new DataPoint(i, points[i].Y + 1);
+            view.InvalidatePlot(true);
         }
     }
 }
