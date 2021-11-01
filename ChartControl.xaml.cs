130c8e0dfeb66672f0dad7c3c28e52882ca4287c
Dmitry Chegodaev <cheg15032001@gmail.com>
2021-11-02 00:19:20 +0300

    add perf counter

--- ChartControl.xaml.cs
+++ ChartControl.xaml.cs
@@ -22,16 +22,16 @@ namespace FourierTransas
         public ChartControl()
         {
             InitializeComponent();
-            var rc = new SkiaRenderContext() {SkCanvas = new SKCanvas(new SKBitmap(1000, 700))};
+            SkiaRenderContext rc = new SkiaRenderContext() {SkCanvas = new SKCanvas(new SKBitmap(1000, 700))};
             rc.RenderTarget = RenderTarget.Screen;
 
-            var models = new FFTModel[]
+            FFTModel[] models = new FFTModel[]
             {
                 new(2000, 15),
                 new(6000, 35),
                 new(4400, 65)
             };
-            var charts = new PlotView[]
+            plots = new PlotView[]
             {
                 Chart0,
                 Chart1,
@@ -39,66 +39,81 @@ namespace FourierTransas
             };
             for (var i = 0; i < 3; i++)
             {
-                charts[i].DataContext = models[i];
-                charts[i].Model = models[i].Plot;
-                (charts[i].Model as IPlotModel).Render(rc, charts[i].Model.PlotArea);
+                plots[i].DataContext = models[i];
+                plots[i].Model = models[i].Plot;
+                (plots[i].Model as IPlotModel).Render(rc, plots[i].Model.PlotArea);
             }
+            points = new List<DataPoint>[]
+            {
+                (Chart0.Model.Series[0] as LineSeries).Points,
+                (Chart1.Model.Series[0] as LineSeries).Points,
+                (Chart2.Model.Series[0] as LineSeries).Points,
+            };
         }
 
         private bool flag = false;
         private Timer timer = new Timer(100);
-
+        private PlotView[] plots;
+        List<DataPoint>[] points;
+        private PerformanceCounter _counter;
+        
         //debug
         // sync 300-400 ms 315 в конце
         // parallel 360-400
-        
+
         //release
         // sync 50-67ms
         // parallel 80-90ms
-        
+
         private void Update_Click(object sender, RoutedEventArgs e)
         {
             flag = !flag;
-
-            var charts = new PlotView[] {Chart0, Chart1, Chart2};
-            var points = new List<DataPoint>[3];
-            for (var i = 0; i < 3; i++) points[i] = (charts[i].Model.Series[0] as LineSeries).Points;
-            var length = points[0].Count;
-            var step = 1;
-            var r = new Random();
-            double sum = 0;
-            int k = 0;
-            // optional: if (2n click) then timer stop
+            _counter = new PerformanceCounter("Process", "% Processor Time", "_Total");
             timer.Enabled = flag;
-            timer.Elapsed += (obj, ev) =>
+            if (flag)
             {
-                double[] gen = Generate.Sinusoidal(length, length * 2, r.Next(0, 199999), r.Next(0, 100));
-                var complex = new Complex[length];
+                timer.Elapsed += UpdatePlot;
+                timer.Elapsed += CheckCPUUsage;
+            }
+        }
+
+        private void UpdatePlot(object sender, ElapsedEventArgs e)
+        {
+            Random r = new Random();
+
+            int length = points[0].Count;
+            double[] gen = Generate.Sinusoidal(length, length * 2, r.Next(0, 199999), r.Next(0, 100));
+            Complex[] complex = new Complex[length];
+            for (int j = 0; j < length; j++)
+            {
+                complex[j] = new Complex(gen[j], 0);
+            }
+
+            Fourier.Forward(complex, FourierOptions.NoScaling);
+            for (int j = 0; j < length; j++)
+            {
+                gen[j] = Math.Sqrt(Math.Pow(complex[j].Real, 2) + Math.Pow(complex[j].Imaginary, 2)) * 2 / length;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
                 for (int j = 0; j < length; j++)
                 {
-                    complex[j] = new Complex(gen[j], 0);
+                    points[i][j] = new DataPoint(points[i][j].X, points[i][j].Y + gen[j]);
                 }
 
-                Fourier.Forward(complex, FourierOptions.NoScaling);
-                for (int j = 0; j < length; j++)
-                {
-                    gen[j] = Math.Sqrt(Math.Pow(complex[j].Real, 2) + Math.Pow(complex[j].Imaginary, 2)) * 2 / length;
-                }
-                var w = new Stopwatch();
-                w.Start();
-                for(int i= 0; i<3; i++)
-                {
-                    for (int j = 0; j < length; j += step)
-                    {
-                        points[i][j] = new DataPoint(points[i][j].X, points[i][j].Y + gen[j]);
-                    }
-                    charts[i].InvalidatePlot(true);
-                }
-                w.Stop();
-                sum += w.ElapsedMilliseconds;
-                k++;
-                Console.WriteLine(sum/k);
-            };
+                plots[i].InvalidatePlot(true);
+            }
+        }
+
+        private void CheckCPUUsage(object sender, ElapsedEventArgs e)
+        {
+            // todo: fix to app cpu usage
+            double limit = 300.0; // measure?
+            if (_counter.NextValue() > limit)
+            {
+                timer.Interval *= (limit / _counter.NextValue());
+            }
         }
     }
 }
