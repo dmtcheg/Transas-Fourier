6e6731793ba677e873bc92287c3a1766bcd0dcf2
Dmitry Chegodaev <cheg15032001@gmail.com>
2021-11-04 15:08:57 +0300

    fix threads plot & process priority

--- ChartControl.xaml.cs
+++ ChartControl.xaml.cs
@@ -13,7 +13,6 @@ using OxyPlot.SkiaSharp;
 using SkiaSharp;
 using System.Timers;
 using System.Windows.Threading;
-using System.Xml.XPath;
 using MathNet.Numerics;
 using MathNet.Numerics.IntegralTransforms;
 
@@ -24,14 +23,11 @@ namespace FourierTransas
         private PlotView[] plots;
         List<DataPoint>[] points;
         private int length;
-        private bool flag = false;
-        private System.Timers.Timer timer = new System.Timers.Timer(100);
-
         PerformanceCounter _counter =
             new PerformanceCounter("Process", "% Processor Time", Process.GetCurrentProcess().ProcessName);
-
+        private DispatcherTimer _dTimer;
         private Random r = new Random();
-        private float limit = 30;
+        private float cpuLimit = 30;
 
         public ChartControl()
         {
@@ -47,9 +43,9 @@ namespace FourierTransas
             };
             plots = new PlotView[]
             {
-                Chart0,
-                Chart1,
-                Chart2
+                PlotView0,
+                PlotView1,
+                PlotView2
             };
             for (var i = 0; i < 3; i++)
             {
@@ -60,50 +56,32 @@ namespace FourierTransas
 
             points = new List<DataPoint>[]
             {
-                (Chart0.Model.Series[0] as LineSeries).Points,
-                (Chart1.Model.Series[0] as LineSeries).Points,
-                (Chart2.Model.Series[0] as LineSeries).Points,
+                (PlotView0.Model.Series[0] as LineSeries).Points,
+                (PlotView1.Model.Series[0] as LineSeries).Points,
+                (PlotView2.Model.Series[0] as LineSeries).Points,
             };
             length = points[0].Count;
 
             var usage = new PlotModel() {Title = "cpu usage"};
-            //var threadCollection = Process.GetCurrentProcess().Threads;
-            // for (int i = 0; i < threadCollection.Count; i++)
-            // {
-            //     threadSeries.Add(threadCollection[i].Id, i); // <thread id, series index>
-            //     usage.Series.Add(new LineSeries());
-            // }
             usage.Series.Add(new LineSeries());
-
-            UsageChart.Model = usage;
-            ((IPlotModel) UsageChart.Model).Render(rc, UsageChart.Model.PlotArea);
+            CpuPlotView.Model = usage;
+            ((IPlotModel) CpuPlotView.Model).Render(rc, CpuPlotView.Model.PlotArea);
         }
 
-        delegate void ThreadDelegate(PlotView view);
         private void Update_Click(object sender, RoutedEventArgs e)
         {
-            flag = !flag;
-            timer.Enabled = flag;
-            if (flag)
-            {
-                timer.Elapsed += UpdatePlot;
-                //timer.Elapsed += CPUUsagePlot;
-                timer.Elapsed += CheckCPULimit;
-            }
-            //timer.AutoReset = false;
-            // ThreadDelegate t = CPUUsagePlot;
-            // Dispatcher.Invoke(t, TimeSpan.FromMilliseconds(100), UsageChart);
-
-            DispatcherTimer dTimer = new DispatcherTimer();
-            dTimer.Interval = TimeSpan.FromMilliseconds(100);
-            dTimer.Tick += CpuUsage;
-            dTimer.Start();
+            _dTimer = new DispatcherTimer(DispatcherPriority.Render);
+            _dTimer.Interval = TimeSpan.FromMilliseconds(100);
+            _dTimer.Tick += SignalPlot;
+            _dTimer.Tick += CpuUsagePlot;
+            _dTimer.Tick += CheckCPULimit;
+            _dTimer.Start();
         }
 
-        private void UpdatePlot(object sender, ElapsedEventArgs e)
+        private void SignalPlot(object sender, EventArgs e)
         {
             _counter.NextValue();
-            
+
             double[] gen = Generate.Sinusoidal(length, length * 2, r.Next(0, 199999), r.Next(0, 100));
             Complex[] complex = new Complex[length];
             for (int j = 0; j < length; j++) complex[j] = new Complex(gen[j], 0);
@@ -128,39 +106,40 @@ namespace FourierTransas
         private int x = 0;
         Dictionary<int, int> threadSeries = new Dictionary<int, int>(); // <thread id, LineSeries>
 
-        private void CpuUsage(object sender, EventArgs e)
+        private void CpuUsagePlot(object sender, EventArgs e)
         {
             var process = Process.GetCurrentProcess();
             ProcessThreadCollection threadCollection = process.Threads;
             foreach (ProcessThread thread in threadCollection)
-            { 
+            {
                 var point = new DataPoint(x,
-                    _counter.NextValue() * (thread.UserProcessorTime/process.UserProcessorTime));
-            
+                    _counter.NextValue()/Environment.ProcessorCount * (thread.UserProcessorTime / process.UserProcessorTime));
+
                 if (threadSeries.ContainsKey(thread.Id))
                 {
-                    (UsageChart.Model.Series[threadSeries[thread.Id]] as LineSeries).Points.Add(point);
+                    (CpuPlotView.Model.Series[threadSeries[thread.Id]] as LineSeries).Points.Add(point);
                 }
                 else
                 {
-                    int i = UsageChart.Model.Series.Count;
+                    int i = CpuPlotView.Model.Series.Count;
                     threadSeries.Add(thread.Id, i);
                     var s = new LineSeries();
                     s.Points.Add(point);
-                    UsageChart.Model.Series.Add(s);
+                    CpuPlotView.Model.Series.Add(s);
                 }
             }
+
             x++;
-            UsageChart.InvalidatePlot(true);
+            CpuPlotView.InvalidatePlot(true);
         }
 
-        private void CheckCPULimit(object sender, ElapsedEventArgs e)
+        private void CheckCPULimit(object sender, EventArgs e)
         {
-            double v = _counter.NextValue();
-            if (v - limit > 5)
-                timer.Interval += 100;
-            else if (limit - v > 5)
-                timer.Interval -= 50;
+            double v = _counter.NextValue()/Environment.ProcessorCount;
+            if (Math.Abs(v - cpuLimit) > 7)
+            {
+                Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.RealTime;
+            }
         }
     }
 }
